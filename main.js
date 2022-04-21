2daab4b7126f314557560e85a2a02545c6791618
Abzed <ibraabzed@gmail.com>
2022-04-21 10:31:52 +0300

    commit tree be518f80a524692c47c8e3b3e8db413e176a2996

--- main.js
+++ main.js
@@ -1,9 +1,39 @@
 // Defining text characters for the empty and full hearts for you to use later.
 const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
+const modal = document.getElementById('modal')
 
 // Your JavaScript code goes here!
+document.addEventListener('DOMContentLoaded', changeheart)
 
+function changeheart(){
+  const btn = document.querySelector('.like-glyph')
+  btn.addEventListener('click', () => {
+    mimicServerCall().then(res => {
+      if(btn.className == 'like-glyph'){
+        btn.innerHTML = FULL_HEART
+        btn.className = 'activated-heart like-glyph'
+        console.log(res);
+      }
+      else{
+        btn.className = 'like-glyph'
+        btn.innerHTML = EMPTY_HEART
+      }
+    }).catch(error => {
+      btn.className = 'like-glyph'
+      btn.innerHTML = EMPTY_HEART
+      modal.className = ''
+      setTimeout(() => modal.className= 'hidden', 3000)
+      console.log(error);
+    })
+  })
+}
+// btn.addEventListener('click', () => {
+//   if(btn.className == 'activated-heart like-glyph'){
+//     btn.className = 'like-glyph'
+//     btn.innerHTML = EMPTY_HEART
+//   }
+// })
 
 
 
